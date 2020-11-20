ffbb69d6d9fe29c7f2d23b6ad5ab80551c2d4f8f
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-20 10:25:24 +0100

    RFB-04: Added form to storepicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
-	render() {
-		return <p>Hello</p>
+	render()
+	{
+		return (
+			<form action="" className="store-selector">
+				<h2>Please Enter a store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store</button>
+			</form>
+		)
 	}
 }
 
