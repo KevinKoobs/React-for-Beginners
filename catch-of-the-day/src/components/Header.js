a4d5342475fa73e74f320145d0390300641ddfbd
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-20 11:05:41 +0100

    RFB-08: Code comment and Router.js for lesson 9

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,17 +1,18 @@
 import React from "react";
 
+// This is a stateless component because it only needs the props, no other functionality
 const Header = (props) => (
 	<header className="top">
 		<h1>
 			Catch
 			<span className="ofThe">
-				        <span className="of">Of</span>
-				        <span className="the">The</span>
-					</span>
+		        <span className="of">Of</span>
+		        <span className="the">The</span>
+			</span>
 			Day
 		</h1>
 		<h3 className="tagline">
-			<span>{ props.tagline }</span>
+			<span>{props.tagline}</span>
 		</h3>
 	</header>
 )
---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,5 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const Router = () => {
+
+}
