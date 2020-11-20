9f0ca657a095b0756953d5315def86dad03501c2
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-20 11:51:49 +0100

    RFB-12: Use push state to change URL in an instance

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,28 @@ import React from 'react';
 import { getFunName } from '../helpers'
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+	goToStore = (event) => {
+		// Stop form from submitting
+		event.preventDefault();
+		// Get storeName from input
+		const storeName = this.myInput.current.value;
+		// Reroute user to /store/storeName
+		this.props.history.push(`/store/${storeName}`);
+	}
+
 	render()
 	{
 		return (
-			<form action="" className="store-selector">
+			<form action="" className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please Enter a store</h2>
-				<input type="text" defaultValue={getFunName()} required placeholder="Store Name"/>
+				<input
+					type="text"
+					ref={this.myInput}
+					required
+					placeholder="Store Name"
+					defaultValue={getFunName()}
+				/>
 				<button type="submit">Visit Store</button>
 			</form>
 		)
