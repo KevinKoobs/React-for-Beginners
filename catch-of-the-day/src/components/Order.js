100da0ab823556edcf126b1a4b393f59b00e9317
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-21 10:51:51 +0100

    RFB-17: Show fish in your order if you add it

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,45 @@
 import React from "react";
+import {formatPrice} from "../helpers";
 
 class Order extends React.Component {
-	render() {
+
+	renderOrder = (key) => {
+		const fish = this.props.fishes[key];
+		const amount = this.props.order[key];
+		const isAvailable = fish && fish.status === 'available';
+		if(isAvailable) {
+			return (
+				<li key={key}>
+					{amount} lbs {fish.name}
+
+					{formatPrice(amount * fish.price)}
+				</li>
+			)
+		}
+	};
+
+	render()
+	{
+		const orderIds = Object.keys(this.props.order);
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const amount = this.props.order[key];
+			const available = fish && fish.status === 'available';
+			if (available) {
+				return prevTotal + (amount * fish.price);
+			}
+			return prevTotal;
+		}, 0);
 		return (
-			<div className="order">Order</div>
+			<div className="order-wrap">
+				<h2>Your Order</h2>
+				<ul className="order">
+					{orderIds.map(this.renderOrder)}
+				</ul>
+				<div className="total">
+					Total: <strong>{formatPrice(total)}</strong>
+				</div>
+			</div>
 		)
 	}
 }
