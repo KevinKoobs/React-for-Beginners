5eea76c712ab5eccc2bf38110a56f384e5b669e0
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-21 11:07:02 +0100

    RFB-18: Setup base authentication

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,5 @@
+import Rebase from 're-base';
+import firebase from "firebase";
+import creds from './env'
+
+const firebaseApp = firebase.initializeApp(creds);
---
+++ catch-of-the-day/src/env.template.js
@@ -0,0 +1,5 @@
+const creds = {
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+}
