8c1532acb7ca20fef42947c60844ae6beb1d7cdb
Kevin Koobs <kevinkoobs@impres.nl>
2020-11-23 19:45:33 +0100

    RFB-23: Use PropTypes to make tagline in Header a string

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 // This is a stateless component because it only needs the props, no other functionality
 const Header = (props) => (
@@ -16,4 +17,9 @@ const Header = (props) => (
 		</h3>
 	</header>
 )
+
+Header.propTypes = {
+	tagline: PropTypes.string.isRequired
+}
+
 export default Header;
